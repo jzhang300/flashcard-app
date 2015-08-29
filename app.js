b01ab352e7d390ab2765a32ace82073e4b3a72ba
JAMES ZHANG <jzhang300@us.ibm.com>
2015-08-29 15:58:53 -0500

    clean app.js

--- app.js
+++ app.js
@@ -19,31 +19,16 @@
 var express  = require('express'),
   app        = express(),
   path       = require('path'),
-  bluemix    = require('./config/bluemix'),
   extend     = require('util')._extend,
-  watson     = require('watson-developer-cloud'),
   favicon    = require('serve-favicon');
 
 // Bootstrap application settings
 require('./config/express')(app);
 
+// hook up favicon
 app.use(favicon(__dirname + '/public/images/favicon.ico'));
 
-// if bluemix credentials exists, then override local
-var credentials =  extend({
-  url: '<url>',
-  username: '<username>',
-  password: '<password>',
-  version: 'v1'
-}, bluemix.getServiceCreds('<service name>')); // VCAP_SERVICES, enter service name from node_modules/watson-developer-cloud/lib/index.js
-
-/*
-code goes in between here
-*/
-
-// error-handler settings
-require('./config/error-handler')(app);
-
+// start ports
 var port = process.env.VCAP_APP_PORT || 3000;
 app.listen(port);
 console.log('listening at:', port);
